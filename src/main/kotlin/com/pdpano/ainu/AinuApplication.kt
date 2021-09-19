138c495feb01199143ad57dd824e4cc22b121f83
pdpan0 <lm.m4rtins@gmail.com>
2021-09-18 23:14:58 -0300

    Estrutura do inicial do projeto.

---
+++ src/main/kotlin/com/pdpano/ainu/AinuApplication.kt
@@ -0,0 +1,11 @@
+package com.pdpano.ainu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AinuApplication
+
+fun main(args: Array<String>) {
+	runApplication<AinuApplication>(*args)
+}
---
+++ src/test/kotlin/com/pdpano/ainu/AinuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pdpano.ainu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AinuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
