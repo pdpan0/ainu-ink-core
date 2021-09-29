10dfd8a9d78d4cda95922a7b562ce94663d8d3d3
Lucas Martins <lucas.martins@VALEMOBI.CORP@VMNOTE-258.valemobi.corp>
2021-09-29 11:07:33 -0300

    BUILD (build.gradle.kts) -> Conexão com base de dados H2

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.h2database:h2:1.4.200")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("org.postgresql:postgresql")
+//	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
