d69ddb0c32884339cb4be33058b35692cecb33fa
Lucas Martins <lucas.martins@VALEMOBI.CORP@VMNOTE-258.valemobi.corp>
2021-09-29 23:10:37 -0300

    BUILD (build.gradle.kts) -> Adicionando a dependência do Spek Framework ao build.gradle

---
+++ src/main/kotlin/com/pdpano/ainu/models/User.kt
@@ -0,0 +1,4 @@
+package com.pdpano.ainu.models
+
+class User {
+}
---
+++ src/test/kotlin/com/pdpano/ainu/models/UserTest.kt
@@ -0,0 +1,48 @@
+package com.pdpano.ainu.models
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class UserTest {
+
+    @Test
+    fun getId() {
+    }
+
+    @Test
+    fun getName() {
+    }
+
+    @Test
+    fun getBirth() {
+    }
+
+    @Test
+    fun getEmail() {
+    }
+
+    @Test
+    fun getCpf() {
+    }
+
+    @Test
+    fun is_active() {
+    }
+
+    @Test
+    fun getCreated_at() {
+    }
+
+    @Test
+    fun testToString() {
+    }
+
+    @Test
+    fun testHashCode() {
+    }
+
+    @Test
+    fun testEquals() {
+    }
+}
