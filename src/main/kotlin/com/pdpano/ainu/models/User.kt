24bd9c695ea9a38c91a874669f6f45dee717b733
Lucas Martins <lucas.martins@VALEMOBI.CORP@VMNOTE-258.valemobi.corp>
2021-09-29 23:13:13 -0300

    FEAT (User.kt) -> Criando entidade do usuário na aplicação.

--- src/main/kotlin/com/pdpano/ainu/models/User.kt
+++ src/main/kotlin/com/pdpano/ainu/models/User.kt
@@ -1,4 +1,21 @@
 package com.pdpano.ainu.models
 
-class User {
-}
+import java.time.LocalDate
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+data class User(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    @Column(nullable = false)
+    val name: String,
+    @Column(nullable = false)
+    val birth: LocalDate,
+    @Column(nullable = false)
+    val email: String,
+    @Column(nullable = false)
+    val cpf: String,
+    val is_active: Boolean = true,
+    val created_at: LocalDateTime = LocalDateTime.now()
+)
