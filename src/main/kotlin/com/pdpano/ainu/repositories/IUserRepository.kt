7e14572a4056b97a21306a5b3104dde226d55422
pdpan0 <lm.m4rtins@gmail.com>
2021-10-04 23:44:04 -0300

    FEAT (IUserRepository.kt) -> Adicionando os metódos (findUserByEmail, existsUserByCpf, existsUserByEmail)

--- src/main/kotlin/com/pdpano/ainu/repositories/IUserRepository.kt
+++ src/main/kotlin/com/pdpano/ainu/repositories/IUserRepository.kt
@@ -5,4 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface IUserRepository: JpaRepository<User, Long>
+interface IUserRepository: JpaRepository<User, Long> {
+    fun existsUserByCpf(cpf: String): Boolean
+
+    fun existsUserByEmail(email: String): Boolean
+
+    fun findUserByEmail(email: String): List<User>
+}
