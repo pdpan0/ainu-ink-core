0d6d021ca124e785517f51bcf098cf9e2c839765
pdpan0 <lm.m4rtins@gmail.com>
2021-10-04 23:46:11 -0300

    TEST (UserTests.kt) -> Validando a criação de usuários.

--- src/test/kotlin/com/pdpano/ainu/models/UserTests.kt
+++ src/test/kotlin/com/pdpano/ainu/models/UserTests.kt
@@ -1,6 +1,12 @@
 package com.pdpano.ainu.models
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.pdpano.ainu.services.UserService
+import org.slf4j.Logger
+import com.pdpano.ainu.utils.LogUtils
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -17,14 +23,35 @@ import java.time.LocalDate
 class UserTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
+    @Autowired
+    private lateinit var service: UserService
+
+    private val log: Logger = LogUtils().getLogger(javaClass)
+
+    val user = User(0L, "Lucas", LocalDate.of(2001, 2, 16), "lucas@ainu.com.br", "1233211233")
+
+    @BeforeEach
+    fun init() {
+        log.info("--- Antes ---")
+        service.createUser(user)
+    }
+
+    @AfterEach
+    fun destroy() {
+        log.info("--- Depois ---")
+    }
 
     @Test
     fun `test criando usuario`() {
-        val user = User(0L,"Lucas", LocalDate.of(2001,2,16),"lucas@ainu.com.br", "1233211233")
-        val json = ObjectMapper().findAndRegisterModules().writeValueAsString(user)
+        val newUser = User(0L, "Lucas", LocalDate.of(2001, 2, 16), "lucas-test@ainu.com.br", "1233211233X")
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/v1/users").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(json))
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andDo(MockMvcResultHandlers.print())
+        val json = ObjectMapper().findAndRegisterModules().writeValueAsString(newUser)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/v1/users").accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON).content(json)
+        ).andExpect(MockMvcResultMatchers.status().isOk).andDo(MockMvcResultHandlers.print())
+
+        Assertions.assertNotNull(service.getUserByEmail(newUser.email))
     }
 }
