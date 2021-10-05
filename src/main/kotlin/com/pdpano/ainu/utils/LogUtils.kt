f2fb07e416404bc551e2be2d3b5cbd356c84265f
pdpan0 <lm.m4rtins@gmail.com>
2021-10-04 23:42:49 -0300

    REFACT (LogUtils.kt) -> Alterando nome Logger.kt -> LogUtils.kt devido conflitos.

--- src/main/kotlin/com/pdpano/ainu/utils/LogUtils.kt
+++ src/main/kotlin/com/pdpano/ainu/utils/LogUtils.kt
@@ -3,6 +3,6 @@ package com.pdpano.ainu.utils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class Logger {
+class LogUtils {
     fun getLogger(forClass: Class<*>): Logger = LoggerFactory.getLogger(forClass)
 }
