f0f77c19b9ae52c76985327f98fd4fec6d1cda9f
pdpan0 <lm.m4rtins@gmail.com>
2021-10-04 23:41:44 -0300

    FEAT (UserService.kt) -> Adicionando validação e novo metódo (getUserByEmail).

--- src/main/kotlin/com/pdpano/ainu/services/UserService.kt
+++ src/main/kotlin/com/pdpano/ainu/services/UserService.kt
@@ -2,15 +2,19 @@ package com.pdpano.ainu.services
 
 import com.pdpano.ainu.models.User
 import com.pdpano.ainu.repositories.IUserRepository
-import com.pdpano.ainu.utils.Logger
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(private val repository: IUserRepository) {
-    private val logger: org.slf4j.Logger = Logger().getLogger(javaClass)
 
     fun createUser(user: User): Long {
-        logger.info("passou aqui")
+        if (repository.existsUserByCpf(user.cpf))
+            throw IllegalArgumentException("cpf já existe.")
+        if (repository.existsUserByEmail(user.email))
+            throw IllegalArgumentException("email já existe.")
+
         return repository.save(user).id
     }
+
+    fun getUserByEmail(email: String) = repository.findUserByEmail(email)
 }
